9d02a8d91c9fc78ea1c986a1fac4e1b02e3a820d
Tiana Hayden <tiana.hayden@me.com>
2018-10-18 10:02:37 -0700

    created store in rootStore.js file. Added provider to the index.jsx

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -8,7 +8,7 @@ export default class App extends React.Component {
     return (
       <div className='container'>
         <div className='jumbotron' >
-          <h1 className='display-3 text-center'>Test Tracker</h1>
+          <h1 className='display-3 text-center'>Budget Tracker</h1>
         </div>
         <div className='row'>
           <div className='col-12 col-md-6 mb-4'>
